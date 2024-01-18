5e43ac9f27db72b05409c6ac41512231cc09f952
Abdurrahman <asahmed93@gmail.com>
2024-01-17 20:21:40 -0500

    Commit 2: fixed time

--- src/Components/DatePicker.js
+++ src/Components/DatePicker.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
 const DatePicker = ({ handleDateChange, date }) => {
-    useEffect(() => {
-        console.log(date.toString());
-    }, [date]);
+    const [selectedDate, setSelectedDate] = useState(date);
+
+    const handleInputChange = (e) => {
+        const inputDate = new Date(e.target.value + 'T00:00:00Z'); // Ensure consistent time
+        setSelectedDate(inputDate);
+        handleDateChange(inputDate);
+    };
 
     return (
         <div>
-            <input type="date" onChange={(e) => handleDateChange(e.target.value)} />
-            <p>Selected Date: {date.toLocaleDateString("en-US")}</p>
+            <input type="date" onChange={handleInputChange} value={selectedDate.toISOString().split('T')[0]} />
+            <p>Selected Date: {selectedDate.toISOString().split('T')[0]}</p>
         </div>
     );
 };
